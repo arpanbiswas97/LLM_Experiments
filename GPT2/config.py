aee0835e9230d08b3285449172c1e4e1fcdc28a5
Arpan Biswas <arpanbiswas97@gmail.com>
2024-05-15 23:48:35 +0530

    Update config

--- GPT2/config.py
+++ GPT2/config.py
@@ -9,13 +9,13 @@ class GPT2Config(BaseModel):
     train_path: Path = Path("TRAIN_DATA")
     test_path: Path = Path("TEST_DATA")
     model_weights_path: Path = Path("CHECKPOINTS/gpt2")
-    epochs: int = 20
-    batch_size: int = 10
+    epochs: int = 100
+    batch_size: int = 40
     seq_len: int = 40
     d_model: int = 512
     d_mlp: int = 2048
     heads: int = 4
-    N: int = 2
+    N: int = 4
     dropout: float = 0.9
     preload: str = "latest"
-    lr: float = 0.2
+    lr: float = 0.1
